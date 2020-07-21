60904dc4d77567ad1ef2f1e751cf03c87654b72b
unknown <prakashbahadursingh2@gmail.com>
2020-07-21 02:56:58 -0700

    login router created also session sonfig set up complete to create encrypted value, to set up session expiration, to send cookie only over https, to prevent client to acces newly created cokie, and also set up time to check and remove expired cookies. also logout router created but not tested

--- api/server.js
+++ api/server.js
@@ -4,25 +4,44 @@ const morgan = require("morgan");
 const cors = require("cors");
 
 const bcryptjs = require("bcryptjs");
-// const session = require("express-session");
-// const KnexSessionStore = require("connect-session-knex")(session);
+const session = require("express-session");
+const KnexSessionStore = require("connect-session-knex")(session);
 
 
 const usersRouter = require("../users/users-router.js")
 const authRouter = require("../auth/auth-router.js")
-// const dbConnection = require("../database/connection.js");
-// const authenticate = require("../auth/authenticate-middleware.js");
+const dbConnection = require("../database/connection.js");
+const authenticate = require("../auth/authenticate-middleware.js");
 
 
 const server = express();
 
+const sessionConfiguration = {
+  name: "monster", // name has default value of sid
+  secret: process.env.SESSION_SECRET || "keep it secret, keep it safe!", //this is the key to create encrypted value
+  cookie: {
+      maxAge: 1000 * 60 * 10, //this will set time for cookie life here 10 min //session expiration
+      secure: process.env.USE_SECURE_COOKIES || false, // send the cookie only over https (secure connections), true in production
+      httpOnly: true, // prevent JS code on client from accessing this cookie // true means client JS can not access this cookie
+  },
+  resave: false,
+  saveUninitialized: true, // read docs, it's related to GDPR compliance
+  store: new KnexSessionStore({
+      knex: dbConnection,
+      tablename: "sessions",
+      sidfieldname: "sid",
+      createtable: true,
+      clearInterval: 1000 * 60 * 30, // time to check and remove expired sessions from database
+  }),
+};
 
+server.use(session(sessionConfiguration)); // enables session support
 server.use(helmet());
 server.use(express.json());
 server.use(morgan("dev"));
 server.use(cors());
 
-server.use("/api/users", usersRouter)
+server.use("/api/users",authenticate, usersRouter)
 server.use("/api/auth", authRouter)
 
 server.get("/", (req, res) => {
--- auth/auth-router.js
+++ auth/auth-router.js
@@ -21,4 +21,42 @@ router.post("/register", (req, res) => {
         })
 })
 
+router.post("/login", (req, res) => {
+    const {username, password} = req.body;
+
+    Users.findBy({ username })
+        .then(users => {
+            const user = users[0]
+
+            if(user && bcryptjs.compareSync(password, user.password)){
+                // store the session to the database
+                // produce a cookie and store the session id inside the cookie
+                // send back the cookie with the session id to the client
+                req.session.loggedIn = true;
+                req.session.username = user.username;
+
+                res.status(200).json({ message: "welcome!!", session:req.session })
+            }else {
+                res.status(401).json({message: "invalid credentials"})
+            }
+        })
+        .catch(error => {
+            res.status(500).json({ error: error.message})
+        })
+})
+
+router.get("/logout", (req, res) => {
+    if (req.session) {
+        req.session.destroy(err => {
+            if (err) {
+                res.status(500).json({ message: "error logging out, please try later" });
+            } else {
+                res.status(204).end();
+            }
+        });
+    } else {
+        res.status(200).json({ message: "already logged out" });
+    }
+});
+
 module.exports = router
